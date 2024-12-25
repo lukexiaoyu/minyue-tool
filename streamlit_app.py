904f44110f5b7f3f8459cbf526ddaf4e39b005d4
lukexiao <814008000@163.com>
2024-12-25 12:29:33 +0800

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,13 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.button("Press me")
+
+def read_md_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    return content
+
+# 使用示例
+md_content = read_md_file('./wow/zs.md')
+st.write(md_content)
